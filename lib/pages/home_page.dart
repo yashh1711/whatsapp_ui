51b8a7560cbe8db3f64cb077a039b1cf2e191996
Yash Nagar <nagar.yash107@gmail.com>
2023-05-16 18:15:33 +0530

    Finalized the changes

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:whatsapp_ui/widgets/calls_widget.dart';
+import 'package:whatsapp_ui/widgets/chat_widget.dart';
+import 'package:whatsapp_ui/widgets/community_widget.dart';
+import 'package:whatsapp_ui/widgets/status_widget.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -45,107 +49,17 @@ class HomePage extends StatelessWidget {
                     ])
           ],
         ),
-        body: TabBarView(
+        body: const TabBarView(
           children: [
-            const Placeholder(
-              color: Colors.blueGrey,
-            ),
-            ListView.builder(
-              itemCount: 10,
-              itemBuilder: (context, index) => ListTile(
-                leading: const CircleAvatar(
-                  backgroundColor: Colors.grey,
-                  child: Icon(
-                    Icons.person,
-                    color: Colors.white,
-                  ),
-                ),
-                title: Text('Home ${index + 1}'),
-                subtitle: const Text('Waiting to receive this message'),
-                trailing: const Text('2:12 PM'),
-              ),
-            ),
-            ListView.builder(
-                itemCount: 10,
-                itemBuilder: (context, index) {
-                  if (index == 0) {
-                    return Column(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        ListTile(
-                          leading: Container(
-                            height: 50,
-                            width: 50,
-                            decoration: BoxDecoration(
-                                shape: BoxShape.circle,
-                                border:
-                                    Border.all(color: Colors.teal, width: 4)),
-                            child: const CircleAvatar(
-                              child: Icon(Icons.person),
-                            ),
-                          ),
-                          title: const Text('My Status'),
-                          subtitle: const Text('Tap to add status'),
-                        ),
-                        const Padding(
-                          padding: EdgeInsets.all(12.0),
-                          child: Text(
-                            'Recent Updates',
-                            style: TextStyle(),
-                          ),
-                        ),
-                      ],
-                    );
-                  } else {
-                    return ListTile(
-                      leading: Container(
-                        height: 50,
-                        width: 50,
-                        decoration: BoxDecoration(
-                            shape: BoxShape.circle,
-                            border: Border.all(color: Colors.teal, width: 4)),
-                        child: const CircleAvatar(
-                          child: Icon(Icons.person),
-                        ),
-                      ),
-                      title: Text('$index Status'),
-                      subtitle: const Text('Today, 10:50 AM'),
-                    );
-                  }
-                }),
-            ListView.builder(
-              itemCount: 10,
-              itemBuilder: (context, index) => ListTile(
-                leading: const CircleAvatar(
-                  backgroundColor: Colors.grey,
-                  child: Icon(
-                    Icons.person,
-                    color: Colors.white,
-                  ),
-                ),
-                title: Text('Home ${index + 1}'),
-                subtitle: Row(
-                  children: [
-                    Icon(
-                      Icons.call_received_outlined,
-                      color: index % 3 == 0 ? Colors.teal : Colors.red,
-                    ),
-                    const Text('  Today, 12:57 PM')
-                  ],
-                ),
-                trailing: IconButton(
-                    onPressed: () {},
-                    icon: Icon(
-                      index % 2 == 0 ? Icons.phone : Icons.video_call,
-                      color: Colors.teal,
-                    )),
-              ),
-            ),
+            CommunityWidget(),
+            ChatWidget(),
+            StatusWidget(),
+            CallWidget(),
           ],
         ),
         floatingActionButton: FloatingActionButton(
           onPressed: () {},
+          backgroundColor: Colors.teal,
           child: const Icon(Icons.message),
         ),
       ),
